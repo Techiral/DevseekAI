03b447bbe1118fb4c1d2394649e36ecddc725565
Techiral <115097129+Techiral@users.noreply.github.com>
2025-02-10 18:24:13 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -25,14 +25,11 @@ export default function RootLayout({ children }) {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                  f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-KH2VS9G3'); // Replace with your GTM ID
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-MXQQBLSZ'); // Replace with your GTM ID
               `,
             }}
           />
@@ -43,7 +40,7 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KH2VS9G3" // Replace with your GTM ID
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MXQQBLSZ" // Replace with your GTM ID
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
